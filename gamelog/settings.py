61143cf4a3900da3925d081ccc040f38cf803eb2
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-07-09 10:14:34 +0100

    Adding South into project and creating initial migrations.

--- gamelog/settings.py
+++ gamelog/settings.py
@@ -124,6 +124,8 @@ INSTALLED_APPS = (
 
     'games',
     'players',
+
+    'south',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
