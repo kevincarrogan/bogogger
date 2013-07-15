409f3d443bd7babc9766c7496947b0c474418fee
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-07-15 09:58:17 +0100

    Fixing and adding static files.

--- gamelog/settings.py
+++ gamelog/settings.py
@@ -1,7 +1,7 @@
 # Django settings for gamelog project.
 import os
 
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -75,6 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -111,7 +112,7 @@ ROOT_URLCONF = 'gamelog.urls'
 WSGI_APPLICATION = 'gamelog.wsgi.application'
 
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_DIR, 'gamelog/templates'),
+    os.path.join(PROJECT_DIR, 'templates'),
 )
 
 INSTALLED_APPS = (
