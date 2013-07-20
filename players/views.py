54614102f54820da23d9f2ec3d79ba61c1d91078
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-07-20 14:25:32 +0100

    Displaying recent games for a player.

--- players/views.py
+++ players/views.py
@@ -2,6 +2,8 @@ from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
+from games.models import GamePlay
+
 from .models import Player
 
 
@@ -11,6 +13,15 @@ class PlayerCreateView(CreateView):
 
 class PlayerDetailView(DetailView):
     model = Player
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(PlayerDetailView, self).get_context_data(*args, **kwargs)
+
+        player = self.get_object()
+
+        ctx['recently_played'] = GamePlay.objects.filter(players=player)[:5]
+
+        return ctx
 
 
 class PlayerListView(ListView):
