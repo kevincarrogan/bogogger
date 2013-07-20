9e30d300e3867d9d86155475b23e6221fb691b78
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-07-20 13:48:36 +0100

    Adding MySQL.

--- gamelog/settings.py
+++ gamelog/settings.py
@@ -14,10 +14,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '%s/gamelog.db' % os.environ.get('TMPDIR'),                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'gamelog',
         # The following settings are not used with sqlite3:
-        'USER': '',
+        'USER': 'gamelog',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
