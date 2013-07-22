054a22a742c563179506cb40f914ac45083ca337
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-07-22 09:42:29 +0100

    Adding top level navigation.

--- games/urls.py
+++ games/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.GameListView.as_view(), name='game_list'),
     url(r'^create/$', views.GameCreateView.as_view(), name='game_create'),
-    url(r'^plays/$', views.GamePlayListView.as_view(), name='game_list'),
+    url(r'^plays/$', views.GamePlayListView.as_view(), name='game_play_list'),
     url(r'^plays/create/$', views.GamePlayCreateView.as_view(), name='game_play_create'),
     url(r'^plays/(?P<pk>\d+)/$', views.GamePlayDetailView.as_view(), name='game_play_detail'),
     url(r'^(?P<slug>.+)/plays/create/$', views.GamePlayCreateFromGameView.as_view(), name='game_play_create_from_game'),
