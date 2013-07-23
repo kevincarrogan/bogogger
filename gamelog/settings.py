69c5a657bbea5b675d9fb6a9c7ff59feec28c28e
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-07-23 13:57:01 +0100

    Setting root as user for DB on development.

--- gamelog/settings.py
+++ gamelog/settings.py
@@ -17,7 +17,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'gamelog',
         # The following settings are not used with sqlite3:
-        'USER': 'gamelog',
+        'USER': 'root',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
 
     'games',
     'players',
+    'ratings',
 
     'south',
 )
