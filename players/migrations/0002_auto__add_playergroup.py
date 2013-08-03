49861defec7ab39f3e71ef1c5ff50d3202b739f1
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-08-03 13:08:30 +0100

    Ensuring migrations run in the correct order.

--- players/migrations/0002_auto__add_playergroup.py
+++ players/migrations/0002_auto__add_playergroup.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ('games', '0001_initial',),
+    )
+
     def forwards(self, orm):
         # Adding model 'PlayerGroup'
         db.create_table(u'players_playergroup', (
