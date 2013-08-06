b7411d486dfa8f5a0287d33bbeeb20dcb5962798
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-08-06 12:57:04 +0100

    Adding leaderboards on group detail page.

--- groups/views.py
+++ groups/views.py
@@ -2,7 +2,7 @@ from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
-from .models import PlayerGroup
+from .models import PlayerGroup, GroupGamePlayerRating
 
 
 class PlayerGroupCreateView(CreateView):
@@ -11,6 +11,22 @@ class PlayerGroupCreateView(CreateView):
 
 class PlayerGroupDetailView(DetailView):
     model = PlayerGroup
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(PlayerGroupDetailView, self).get_context_data(*args, **kwargs)
+
+        leaderboards = []
+
+        group = self.get_object()
+
+        for game in group.games.all():
+            leaderboards.append(
+                (game, GroupGamePlayerRating.objects.filter(game=game, group=group).order_by('-rating__rating'),),
+            )
+
+        ctx['leaderboards'] = leaderboards
+
+        return ctx
 
 
 class PlayerGroupListView(ListView):
