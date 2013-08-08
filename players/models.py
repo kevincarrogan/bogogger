7453932fb4377e073a3c0f5a66c52dfdd46fdde9
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-08-08 19:54:48 +0100

    Creating a player when a new user signs up.

--- players/models.py
+++ players/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from autoslug import AutoSlugField
 
@@ -9,7 +11,7 @@ from autoslug import AutoSlugField
 class Player(models.Model):
     user = models.ForeignKey(get_user_model(), blank=True, null=True)
     slug = AutoSlugField(
-        populate_from=lambda instance: '%s %s' % (instance.first_name, instance.last_name)
+        populate_from='fullname',
     )
 
     # Fields if this isn't tied to a user
@@ -21,6 +23,13 @@ class Player(models.Model):
         max_length=255, blank=True, null=True,
         verbose_name='Last name',
     )
+
+    @property
+    def fullname(self):
+        if self.first_name and self.last_name:
+            return u'%s %s' % (self.first_name, self.last_name)
+        else:
+            return self.user.username
 
     def get_absolute_url(self):
         return reverse('player_detail', args=(), kwargs={'slug': self.slug})
@@ -43,7 +52,10 @@ class Player(models.Model):
         return self.get_from_self_or_user('last_name')
 
     def __unicode__(self):
-        return u'%s %s' % (self.first_name, self.last_name)
+        if self.first_name:
+            return u'%s %s' % (self.first_name, self.last_name)
+        else:
+            return self.slug
 
     def get_current_rating_for_game(self, game):
         try:
@@ -62,3 +74,12 @@ class Player(models.Model):
             return group_ratings[0].rating
         except IndexError:
             return self.get_current_rating_for_game(game)
+
+
+@receiver(post_save, sender=get_user_model())
+def create_player_from_user(sender, **kwargs):
+    user = kwargs['instance']
+
+    Player.objects.create(
+        user=user,
+    )
