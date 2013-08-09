37b693b3a15fbe0ac0bb438d8a0cd867ad967697
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-08-09 13:08:06 +0100

    Adding is staff field and marking is active as false by default.

--- authorisation/models.py
+++ authorisation/models.py
@@ -7,7 +7,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True, db_index=True)
     first_name = models.CharField(max_length=255, null=True, blank=True)
     last_name = models.CharField(max_length=255, null=True, blank=True)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
