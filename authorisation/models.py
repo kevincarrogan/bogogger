a39ccb32efa6bf11d3f882cf7669e8509adb3a24
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-08-09 13:07:21 +0100

    Adding in permission mixin for user model.

--- authorisation/models.py
+++ authorisation/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255, unique=True, db_index=True)
     email = models.EmailField(max_length=255, unique=True, db_index=True)
     first_name = models.CharField(max_length=255, null=True, blank=True)
