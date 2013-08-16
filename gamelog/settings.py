dc84f1b752222b744109e65f4be92757a24dcca3
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-08-16 10:33:40 +0100

    Adding the correct domain names.

--- gamelog/settings.py
+++ gamelog/settings.py
@@ -39,7 +39,7 @@ elif ENVIRONMENT == 'production':
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['gamelogger.herokuapp.com']
+ALLOWED_HOSTS = ['bogogger.herokuapp.com', 'www.bogogger.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
