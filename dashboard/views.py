b21a9c198c84136abc9676adca0a911e2d633eac
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-08-16 09:56:48 +0100

    Redirecting to group list from dashboard as dashboard isn't yet implemented.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,11 @@
-from django.views.generic.base import TemplateView
+from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse
 
 from braces.views import LoginRequiredMixin
 
 
-class DashboardView(LoginRequiredMixin, TemplateView):
+class DashboardView(LoginRequiredMixin, RedirectView):
     template_name = 'dashboard/dashboard.html'
+
+    def get_redirect_url(self):
+        return reverse('player_group_list')
